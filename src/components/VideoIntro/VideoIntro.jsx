9546175b86758dc2f7472d5723d28ff95a67a1d1
psine <patricio.sine@gmail.com>
2024-07-04 15:12:23 -0300

    update video component

--- src/components/VideoIntro/VideoIntro.jsx
+++ src/components/VideoIntro/VideoIntro.jsx
@@ -1,17 +1,16 @@
 import React, { forwardRef } from 'react';
 import styles from './VideoIntro.module.css';
-import PropTypes from 'prop-types';
 
-const VideoIntro = forwardRef(({ videoSrc, autoPlay = true  }, ref) => (
+const VideoIntro = ({  }) => {
+  return (
+    <>
     <video className={styles.video} autoPlay={true} muted loop>
       <source src={'/videos/intro.webm'} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
-));
-
-VideoIntro.propTypes = {
-    videoSrc: PropTypes.string.isRequired,
-  };
+    </>
+  );
+};
 
 export default VideoIntro;
 
